0aa76fdfb316ef4321b46740fa0938f209a5c305
Brandi Burnell <brandiburnell6@gmail.com>
2024-01-07 17:01:13 -0700

    fix bug where p tags were affecting multiple components

--- src/components/History.js
+++ src/components/History.js
@@ -6,42 +6,41 @@ function History() {
     return (
         <div className="history-container">
             <h1 className="page-title">Colorado Backcounty Hut History</h1>
-            <p>
+            <p className="paragraph">
                 For many years the world over, mountain huts have sheltered backcountry travelers such as skiers and mountaineers. In Europe, hundreds of huts hang from mountainsides, and many have been in use for over a century. Likewise, mountain huts are a noble tradition in Canada and the northeast United States. For example, the Carter Notch Hut in New Hampshire was built in 1904, and is perhaps the oldest all-season mountaineering hut still in use in the United States. Early huts in the western U.S. were fewer, but they existed in the northwest and California.
             </p>
-            <p>
+            <p className="paragraph">
                 The word “hut” originally meant a small shack, often hastily built and impermanent. With the advent of modern backcountry recreation the word has taken on a new meaning, and describes everything from one-room shelters to structures that include multiple bedrooms, indoor plumbing, and comfortable heating. 
             </p>
-            <p>
+            <p className="paragraph">
                 In the 1940s in Colorado, a chain of huts (used as sheep herder shelters and Forest Service guard shacks) cut through what is now the Maroon Bells-Snowmass Wilderness, and other huts were built near the populous Front Range cities such as Denver. The best known of Colorado’s eastern huts is the Fern Lake Lodge, a cabin in Rocky Mountain National Park used by enthusiastic skiers as early as 1916.
-            </p>
+            </p >
             <img src="https://huts.org/wp-content/uploads/2023/04/soldiers.jpg" alt="soldiers"></img>
-
-            <p>
+            <p className="paragraph">
                 The first true ski hut (that used mostly for a skiing basecamp) in Colorado was probably created in the late 1940s, when Billie Tagert and his friends fixed up an old miner’s cabin at the head of Castle Creek near Aspen. Tagert and his group loved ski mountaineering, but they knew that skiing with a load of camp gear could turn fun into plain toil. The “hut trend” started by Tagert has changed all that. Since those old-timers skied to Fern Lake and Castle Creek, scores of huts have been built in Colorado. They range from primitive to palatial, but have one thing in common: popularity. In the Colorado backcountry, winter camping is now an option rather than a necessity.
             </p>
-            <p>
+            <p className="paragraph">
                 The Castle Creek “Tagert Hut” was subsequently rebuilt, and is now part of the Alfred A. Braun Hut System — Colorado’s first ski oriented hut system. The idea for the Braun hut system was originally voiced by Stuart and Isabel Mace, of Ashcroft, in the early 1950’s. Today seven high mountain cabins provide skiers with nearly endless skiing and mountaineering opportunities in the spectacular Elk Mountain Range. The huts can be skied hut-to-hut or out and back from trailheads in the Aspen area. The Tagert and Green Wilson Huts can also be used in conjunction with the Friends’ Hut, to ski between the towns of Aspen and Crested Butte. The hut system is named after the late Fred Braun who managed it for over twenty years. Braun is also known as the founder of Mountain Rescue Aspen.
             </p>
-            <p>
+            <p className="paragraph">
                 The most extensive of the Colorado ski hut systems is managed by the 10th Mountain Division Hut Association. A nonprofit corporation, “10th Mountain” was formed in the early 1980s by several Aspen skiers, including 10th Mountain Division veteran Fritz Benedict, who cherished the perfect ski touring between Vail and Aspen. From the start the group set their sights on a trail with closely spaced huts, all accessible via intermediate ski touring trails. 
             </p>
-            <p>
+            <p className="paragraph">
                 The first 10th Mountain Division Huts built were the McNamara and Margy’s, completed during the summer of 1982. In the ensuing decade eight more huts were completed, with several more in recent years, for a total of 14 huts owned by 10th Mountain. What’s more, as 10th Mountain grew it became the source for central Colorado hut reservations, and now books 38 huts: 10th Mountain owned (14), Alfred A. Braun Hut System (7), the Friends’ Hut (1), the Summit Huts Association (5), the Grand Huts Association (1), as well as privately owned huts (10).
             </p>
-            <p>
+            <p className="paragraph">
                 The name “10th Mountain” honors the soldiers of the 10th Mountain Division of the U.S. Army. Five of the 10th Mountain Division Huts were built with donations from family and friends to honor soldiers of the 10th Mountain Division who died in World War II.
             </p>
-            <p>
+            <p className="paragraph">
                 In the early 1940s, with the world at the brink of World War II, the U.S. War Department began experimenting with mountain troops. It was soon obvious that having troops trained for the mountains was necessary. A training ground was chosen in Colorado’s Eagle River Valley just to the north of Tennessee Pass. Known as Camp Hale, the site became home to more than 11,000 troops in December 1942.
             </p>
-            <p>
+            <p className="paragraph">
                 The troops trained for about two years at Camp Hale, then moved to Washington and Texas. By January of 1945 the 10th Mountain Division was in Italy, where they played a crucial role in several battles. The men of the 10th developed a strong camaraderie, and many gained a love for Colorado as well. After the war, hundreds of 10th vets returned to Colorado to settle down. With their ski skills, many of these men were key figures in the development of Colorado’s ski industry.
             </p>
-            <p>
+            <p className="paragraph">
                 Did the 10th soldiers use huts? No doubt they used cabins and shelters by opportunity, both in Colorado and in Europe. Their most well documented use of a ski hut is when a small group in 1944 bunked for a night in a cabin near the confluence of Halfmoon Creek and North Halfmoon Creek, during their legendary “Trooper” ski traverse from Leadville to Aspen. The cabin was intact up to 2005, when it burned down.
             </p>
-            <p>
+            <p className="paragraph">
                 ALL TEXT CREDIT TO 10th MOUNTAIN DIVISION
                 <br />
                 <a href="https://huts.org/history-of-the-huts/">HISTORY OF THE HUTS</a>
